8482580d2b8c2347128840b63ef03cc12517bc83
Raphael <matosramosraphael@gmail.com>
2023-07-11 23:37:42 -0500

    33:04

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Componentes
+import { ListProductsComponent } from './components/list-products/list-products.component';
+import { AddEditProductComponent } from './components/add-edit-product/add-edit-product.component';
+
+const routes: Routes = [
+  { path: '', component: ListProductsComponent }, //Cuando el usuario ponga la ruta vacia, va a retornar ese componente
+  { path: 'add', component: AddEditProductComponent }, //Cuando el usuario ponga la ruta add, va a retornar ese componente
+  { path: '**', redirectTo: '', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
