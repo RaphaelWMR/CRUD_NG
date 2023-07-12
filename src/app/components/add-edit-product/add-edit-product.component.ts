0134c7081f959894335120a20dde014a02be26e2
Raphael <matosramosraphael@gmail.com>
2023-07-12 06:53:50 -0500

    1:05:09 front completo

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -25,8 +25,14 @@ export class AddEditProductComponent implements OnInit {
   }
 
   addProduct() {
+    console.log(this.form.value.name);
 
-    console.log(this.form);
+    const product: Product = {
+      name: this.form.value.name,
+      description: this.form.value.description,
+      price: this.form.value.price,
+      stock: this.form.value.stock
+    }
   }
 
 }
