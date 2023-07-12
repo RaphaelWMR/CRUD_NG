01283e9c8c47c9b07f9b7f10ed1ac462cd7b3a58
Raphael <matosramosraphael@gmail.com>
2023-07-12 06:42:20 -0500

    55:53

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Product } from 'src/app/interfaces/product';
 
 @Component({
   selector: 'app-add-edit-product',
   templateUrl: './add-edit-product.component.html',
   styleUrls: ['./add-edit-product.component.css']
 })
-export class AddEditProductComponent {
+export class AddEditProductComponent implements OnInit {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group(
+      {
+        name: ['', Validators.required],//Primer parametro, valor predefinido, si le pongo algo, se autocompleta el form
+        description: ['', Validators.required], // required: no empty
+        price: [null, Validators.required],// si en un campo quiero ple mas de un validator
+        stock: [null, Validators.required]//los pongo en un array
+      }
+    )
+  }
+
+  ngOnInit(): void {
+  }
+
+  addProduct() {
+
+    console.log(this.form);
+  }
 
 }
