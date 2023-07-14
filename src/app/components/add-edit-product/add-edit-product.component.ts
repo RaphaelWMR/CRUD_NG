01f52885189264379ed85d18f8ee3d650718865a
Raphael <matosramosraphael@gmail.com>
2023-07-14 04:30:07 -0500

    2:59:01

--- src/app/components/add-edit-product/add-edit-product.component.ts
+++ src/app/components/add-edit-product/add-edit-product.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Product } from 'src/app/interfaces/product';
 import { ProductService } from 'src/app/services/product.service';
 
@@ -13,11 +13,13 @@ import { ProductService } from 'src/app/services/product.service';
 export class AddEditProductComponent implements OnInit {
   form: FormGroup;
   loading: boolean = false;
-
+  id: number;
+  operacion: string = "Agregar ";
   constructor(private fb: FormBuilder,
     private _productService: ProductService,
     private router: Router,
-    private toastr: ToastrService) {
+    private toastr: ToastrService,
+    private aRouter: ActivatedRoute) {
     this.form = this.fb.group(
       {
         name: ['', Validators.required],//Primer parametro, valor predefinido, si le pongo algo, se autocompleta el form
@@ -26,9 +28,14 @@ export class AddEditProductComponent implements OnInit {
         stock: [null, Validators.required]//los pongo en un array
       }
     )
+    this.id = Number(aRouter.snapshot.paramMap.get('id'));//se obteiene el id del link, cuando es editar
   }
 
   ngOnInit(): void {
+    if (this.id != 0) {
+      //Editar
+      this.operacion = "Editar ";
+    }
   }
 
   addProduct() {
